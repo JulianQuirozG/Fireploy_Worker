f06c9e558a69721477f1b1d3bcf9d0d019d4ff93
Julian <iscalajul@gmail.com>
2025-04-14 17:28:02 -0500

    Feat: crear contanedores bases de datos

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DockerfileService } from './services/docker.service';
 
 async function bootstrap() {
+  const dockerfileService = new DockerfileService();
+  await dockerfileService.setupDatabases();
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3001);
 }
