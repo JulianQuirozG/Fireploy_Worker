168f14a51275ecc0f7000fdec19c16a746965edf
Julian <iscalajul@gmail.com>
2025-04-15 14:57:59 -0500

    Fix: delete carpeta services

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,6 @@ async function bootstrap() {
   const dockerfileService = new DockerfileService();
   await dockerfileService.setupDatabases();
   const app = await NestFactory.create(AppModule);
-  const dockerfileService = new DockerfileService();
-
-  await dockerfileService.setupDatabases();
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
