be0182ec9b8a20c9f81de422a42052f88e7150ac
Julian <iscalajul@gmail.com>
2025-05-15 21:32:23 -0500

    Feat: Springboot

--- src/Services/docker.service.ts
+++ src/Services/docker.service.ts
@@ -246,6 +246,26 @@ export class DockerfileService {
       # Comando para arrancar la aplicación
       CMD ["npm", "start"]
       `,
+      Springboot: `
+      # Etapa 1: Construcción del proyecto con Maven y Java 17
+        FROM maven:3.9.4-eclipse-temurin-17 AS builder
+        WORKDIR /app
+        COPY . .
+        ${envLines}
+        RUN mvn clean package -DskipTests
+
+        # Etapa 2: Imagen de producción con JDK 17 ligero
+        FROM eclipse-temurin:17-jdk-alpine
+        WORKDIR /app
+        COPY --from=builder /app/target/*.jar app.jar
+        ${envLines}
+        EXPOSE ${port}
+        
+        # Variable para pasar flags de JVM si se desea
+        ENV JAVA_OPTS=""
+
+        ENTRYPOINT exec java $JAVA_OPTS -jar app.jar
+`,
     };
 
     // Return the corresponding Dockerfile template for the given technology
