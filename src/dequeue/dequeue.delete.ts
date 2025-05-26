63a2f99d3e82ce58b2c32d9f166bea2d9a85f685
AlvarezJoseR <alvarezreyes246@gmail.com>
2025-05-26 17:02:35 -0500

    Fix: eliminar

--- src/dequeue/dequeue.delete.ts
+++ src/dequeue/dequeue.delete.ts
@@ -12,32 +12,37 @@ export class DeleteProcessor {
 
   @Process({ name: 'delete', concurrency: 1 })
   async deleteProjectJob(job: Job) {
-    const project = job.data;
-    const db = project.base_de_datos;
-    //Delete containers
-    if (project.tipo_proyecto == 'M') {
-      await this.dockerfileService.deleteContainer(
-        `Container-${job.data.project.id}`,
-      );
-    } else {
-      await this.dockerfileService.deleteContainer(
-        `backend_${job.data.project.id}`,
-      );
-      await this.dockerfileService.deleteContainer(
-        `frontend_${job.data.project.id}`,
-      );
-    }
-
-    //Delete project files
-    await this.systemService.deleteFolder(
-      `${process.env.FOLDER_ROUTE}/${project.id}`,
-    );
-
-    //Si tiene eliminar la base de datos
-    if (project.base_de_datos)
-      await this.dockerfileService.deleteDataBase(db.tipo, db.nombre, db.user);
-
     try {
+      const project = job.data;
+      const db = project.base_de_datos;
+      console.log(1);
+      //Delete containers
+      if (project.tipo_proyecto == 'M') {
+        await this.dockerfileService.deleteContainer(
+          `Container-${job.data.project.id}`,
+        );
+      } else {
+        await this.dockerfileService.deleteContainer(
+          `backend_${job.data.project.id}`,
+        );
+        await this.dockerfileService.deleteContainer(
+          `frontend_${job.data.project.id}`,
+        );
+      }
+      console.log(2);
+
+      //Delete project files
+      await this.systemService.deleteFolder(
+        `${process.env.FOLDER_ROUTE}/${project.id}`,
+      );
+      console.log(3);
+      //Si tiene eliminar la base de datos
+      if (project.base_de_datos)
+        await this.dockerfileService.deleteDataBase(
+          db.tipo,
+          db.nombre,
+          db.user,
+        );
     } catch (error) {
       throw new Error(error);
     }
