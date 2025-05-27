02510ea3c86739ddbc5b79d1df80e7a2e9032807
Julian <iscalajul@gmail.com>
2025-05-26 21:39:21 -0500

    manage subdomanins, el propio feature

--- src/Services/nginx.service.ts
+++ src/Services/nginx.service.ts
@@ -18,12 +18,14 @@ export class NginxConfigGenerator {
   private aliases: NginxAlias[];
   private includesDir: string;
   private includeFile: string;
+  private appsDir: string;
 
   constructor(archieveName: string, aliases: NginxAlias[]) {
     this.domain = 'proyectos.fireploy.online';
     this.aliases = aliases;
     this.includesDir = '/etc/nginx/includes';
-    this.includeFile = join(this.includesDir, `app${archieveName}`);
+    this.appsDir = '/etc/nginx/apps';
+    this.includeFile = join(this.includesDir, `${archieveName}`);
   }
 
   async generate() {
@@ -62,6 +64,57 @@ location ${loc} {
     // Escribe el archivo de inclusión
     writeFileSync(this.includeFile, config.trim() + '\n', 'utf8');
     console.log(`📄 Archivo de rutas actualizado: ${this.includeFile}`);
+
+    // Valida y recarga NGINX
+    try {
+      await execSync('nginx -t', { stdio: 'inherit' });
+      await execSync('systemctl reload nginx', { stdio: 'inherit' });
+      console.log('🚀 NGINX recargado correctamente');
+    } catch (e) {
+      console.error('❌ Error al recargar NGINX:', e, ' ErrorCode-005');
+      process.exit(1);
+    }
+  }
+
+  async generateSubDomain() {
+
+    // Asegura que exista el directorio de includes
+    if (!existsSync(this.appsDir)) {
+      mkdirSync(this.appsDir, { recursive: true });
+    }
+
+    for (const alias of this.aliases) {
+
+      const pathFile = join(this.appsDir, alias.path)
+      if (existsSync(pathFile)) {
+        unlinkSync(pathFile);
+      }
+
+      let config = '';
+
+      config += `
+server {
+    listen 443 ssl;
+    server_name ${alias.path}.${this.domain};
+
+    ssl_certificate /etc/letsencrypt/live/proyectos.fireploy.online-0001/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/proyectos.fireploy.online-0001/privkey.pem;
+    include /etc/letsencrypt/options-ssl-nginx.conf;
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+
+    location / {
+        proxy_pass http://${alias.target};
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+}
+`.trim();
+
+      writeFileSync(pathFile, config + '\n', 'utf8');
+      console.log(`📄 Archivo principal de NGINX creado: ${pathFile}`);
+    }
 
     // Valida y recarga NGINX
     try {
